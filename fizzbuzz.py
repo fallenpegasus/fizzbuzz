d28ab260c148c0e18ed6ffe124a648c003bd144e
Mark Atwood <me@mark.atwood.name>
2015-01-04 07:46:52 -0800

    fix non-ascii characters pasted into comment

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,9 +4,9 @@
 
 # http://imranontech.com/2007/01/24/using-fizzbuzz-to-find-developers-who-grok-coding/
 # Write a program that prints the numbers from 1 to 100. But for
-# multiples of three print “Fizz” instead of the number and for the
-# multiples of five print “Buzz”. For numbers which are multiples of
-# both three and five print “FizzBuzz”."
+# multiples of three print Fizz instead of the number and for the
+# multiples of five print Buzz. For numbers which are multiples of
+# both three and five print FizzBuzz."
 
 # todo, make "FizzBuzz" drop out as an emergent case of "Fizz" and "Buzz"
 # todo, generate a lazy evaluated sequence, and then just output the first 100 members
